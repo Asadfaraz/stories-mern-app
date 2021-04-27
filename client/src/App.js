31dc8ae52bd05a7479569e6679b0031db2e4e7e9
Asad Faraz <asadfaraz44@gmail.com>
2021-04-27 13:28:02 +0500

    client simple styles and components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import classes from "./app.module.css";
+
+import memories from "./images/memories.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className={classes.container}>
+      <header>
+        <div className={classes.headItems}>
+          <h2>Memories</h2>
+          <img src={memories} alt="memories" />
+        </div>
+      </header>
+
+      {/*  */}
+      <section className={classes.appComponents}>
+        <article className={classes.postComp}>
+          <Posts />
+        </article>
+        {/*  */}
+        <article className={classes.formComp}>
+          <Form />
+        </article>
+      </section>
     </div>
   );
 };
